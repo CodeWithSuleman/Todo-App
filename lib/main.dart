ebcbd295778ddfdb751c8d416fdc2e16fbbc0109
Abdullah <abdullahsoomro245@gmail.com>
2023-07-03 22:48:06 +0500

    Dummy Branch for learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/loading.dart';
-import 'package:todo_app/todo_list.dart';
+import 'package:todo_app/firebase_options.dart';
 
-void main() {
+import 'screens/todo_list.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: Firebase.initializeApp(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Scaffold(
-              body: Center(
-                child: Text(snapshot.error.toString()),
-              ),
-            );
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Loading();
-          }
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-                scaffoldBackgroundColor: Colors.grey[700],
-                primarySwatch: Colors.pink),
-            home: TodoList(),
-          );
-        });
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoListScreen(),
+    );
   }
 }
--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,6 +1,31 @@
-class Todo {
-  String uid;
-  String title;
-  bool isComplete;
-  Todo({required this.uid, required this.title, required this.isComplete});
+class User {
+  final String id;
+  final String name;
+  final int age;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'age': age,
+      };
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json["id"],
+      name: json["name"],
+      age: json["age"],
+    );
+  }
 }
+
+///{
+///   "id": "1"
+///   "name": "Abdullah"
+///   "age": "22"
+///}
